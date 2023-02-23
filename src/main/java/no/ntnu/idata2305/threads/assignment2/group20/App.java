02f88f5d2bbc76a5aa3146bf18ad5743b79d2dc5
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2023-02-23 16:44:35 +0100

    Initilize maven

---
+++ src/main/java/no/ntnu/idata2305/threads/assignment2/group20/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2305.threads.assignment2.group20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
